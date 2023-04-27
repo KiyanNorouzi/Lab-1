f3772fcf1e8b39784b5c92a058d93b2db54c07a2
Kiyan Norouzi <41071501+KiyanNorouzi@users.noreply.github.com>
2023-04-28 02:52:56 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,24 +1,179 @@
 import csv
+import random
 
-flag = 0
-search = input('Search for: ')
-f = open('result.txt','w')
-with open('civic.csv','r') as csvfile:
-    table = csv.reader(csvfile, delimiter = ';')
-    for row in table:
-        #if (int(row[8]) >= 7000):
-            # print(row[2], ' ', row[8])
-        lower_case = row[2].lower()
-        index = lower_case.find(search.lower())
-        if (index != -1):
-            print(len(row[2]))
+filename = "books-en.csv"  # replace with your file name
 
-            #row[8].replace(',','.')
 
-            flag = 1
-            f.write(row[0] + ' ' + row[2] + ' Стоимость: ' + row[8] + ' рублей.\n')
+###############количество записей и строк####################
 
-    if (flag == 0):
-        print("Nothing found.")
+def EnteryCounter():
+       
+     with open (filename , mode = 'r') as csv_file:
+        csv_reader =  csv.DictReader(csv_file)
+        line_num = 0
+        entry_num = 0
 
-f.close()
+        for row in csv_reader:
+            if line_num == 0:
+                line_num += 1
+                entry_num += 7
+                print(f'Названия столбцов в CSV-файле: {", ".join(row)}')
+
+            line_num += 1
+            entry_num += 7
+        print(f'{line_num} строк обнаружено.')
+        print(f'{entry_num} записей обнаружено.')
+
+EnteryCounter()
+
+###################################
+
+
+
+############количество записей, содержащих строку длиннее 30 символов############
+
+def CharacterReader(num):
+  with open(filename, mode='r') as File:
+    csvReader = csv.DictReader(File)
+    numBooks = 0
+
+    for row in csvReader:
+        counter = 0
+        for c in row["Book-Title"] :  
+            counter+=1  
+        if counter > num :
+              numBooks +=1
+        
+    print(f'количество записей, содержащих строку длиннее {num} символов, равно {numBooks} книгам.')
+
+CharacterReader(30)
+
+###################################
+
+
+
+################Книги от 150 руб###################
+
+def PriceFrom(n):
+         with open (filename , mode = 'r') as csv_file:
+            csvReader = csv.DictReader(csv_file)
+            booksFrom = 0
+
+            for row in csvReader:
+                if row["Price"].isnumeric():
+                    Price = row["Price"]
+                    if int(Price) > n:
+                        booksFrom +=1
+            print(f'количество записей более {n} рублей  {booksFrom} книг.')
+
+PriceFrom(150)
+
+###################################
+
+
+
+
+#################Произвольно выбранные записи##################
+
+def Random(n):
+    
+    with open(filename, "r") as file:
+        csvReader = csv.reader(file)
+        next(csvReader)
+        return(random.choice([line[n] for line in csvReader]) )
+
+def RandomEntries():
+    with open('Произвольно выбранные записи.txt', 'w',encoding="utf-8") as f:
+
+        for x in range(0, 20):
+            AuthorOfBook = "<автор>: "
+            NameOfBook =" - <название>: "
+            YearOfPublish = " - <год>: "
+            lines = [str(AuthorOfBook),str(Random(1)),str(NameOfBook), str(Random(2)),str(YearOfPublish) , str(Random(3))]
+            print(lines)
+            f.writelines(lines)
+            f.write('\n')
+
+RandomEntries()
+
+###################################
+
+
+
+
+###############Уникальные издатели####################
+
+def UniquePublisher():
+
+    # Открываем файл CSV и создаем список словарей, представляющих данные
+    rows = []
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            rows.append(row)
+
+    # теперь мы находим уникальных издателей
+    unique_publishers = set()
+    duplicate_publishers = set()
+    for row in rows:
+        publisher = row["Publisher"]
+        if publisher in unique_publishers:
+            duplicate_publishers.add(publisher)
+        else:
+            unique_publishers.add(publisher)
+
+    #отфильтровываем повторяющиеся строки
+    unique_rows = [row for row in rows if row["Publisher"] not in duplicate_publishers]
+
+    # записываем уникальные строки в выходной файл
+    with open("UniquePublisher.csv", "w", encoding="utf-8", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Downloads", "Price"])
+        for row in unique_rows:
+            writer.writerow([row["ISBN"], row["Book-Title"], row["Book-Author"], row["Year-Of-Publication"], row["Publisher"], row["Downloads"], row["Price"]])
+
+UniquePublisher()
+
+###################################
+
+
+
+################20 самых популярных книг###################
+
+def PopularBook():
+
+    # Открываем файл CSV и создаем список словарей, представляющих данные
+    rows = []
+    with open(filename, "r") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            try:
+                row["Downloads"] = int(row["Downloads"])
+                rows.append(row)
+            except ValueError:
+                pass
+
+   # сортируем строки по загрузкам (в порядке убывания)
+    rows.sort(key=lambda x: x["Downloads"], reverse=True)
+
+    # мы печатаем первые 20 строк, а затем сохранить в файл csv
+    print("{:<15} {:<50} {:<30} {:<20} {:<40} {:<15} {:<10}".format(
+        "ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Downloads", "Price"))
+    for row in rows[:20]:
+        print(
+            row["ISBN"], row["Book-Title"], row["Book-Author"], row["Year-Of-Publication"],
+            row["Publisher"], row["Downloads"], row["Price"])
+        
+
+     
+    top_rows = sorted(rows, key=lambda row: row["Downloads"], reverse=True)[:20]
+    
+    with open("TOP20.csv", "w", encoding="utf-8", newline='') as f:
+        writer = csv.writer(f)
+        writer.writerow(["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Downloads", "Price"])
+        for row in top_rows:
+            writer.writerow([row["ISBN"], row["Book-Title"], row["Book-Author"], row["Year-Of-Publication"], row["Publisher"], row["Downloads"], row["Price"]])
+
+PopularBook()
+
+###################################
